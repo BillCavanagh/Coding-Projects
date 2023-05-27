b2d0715f407c59698e26169ed44786d66691a665
Bill Cavanagh <61886691+BillCavanagh@users.noreply.github.com>
2023-05-27 14:57:48 -0400

    Delete java-projects/chess directory

--- java-projects/chess/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module chess {
-    requires transitive javafx.graphics;
-    requires transitive javafx.controls;
-
-    opens chess to javafx.fxml;
-    exports chess;
-    exports chess.Pieces;
-}
